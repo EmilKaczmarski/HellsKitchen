c4a620d181e4b9cf02cd086282852a1c04bb32f6
olciabrzostek <61205615+olciabrzostek@users.noreply.github.com>
2020-06-14 15:35:07 +0200

    post detail update
    - added segue from post cell to post detail
    - changes in post detail view and controller

--- HellsKitchen/Controller/Wall/PostDetailViewController.swift
+++ HellsKitchen/Controller/Wall/PostDetailViewController.swift
@@ -14,10 +14,24 @@ class PostDetailViewController: UIViewController {
     @IBOutlet weak var date: UILabel!
     @IBOutlet weak var header: UILabel!
     @IBOutlet weak var content: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var radiusView: UIView!
+    
+    let viewModel: PostDetailViewModel = PostDetailViewModel()
     
     var postId: String?
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTitle("hell's kitchen", andImage: #imageLiteral(resourceName: "fire"))
+        radiusView.layer.cornerRadius = 26
+        radiusView.layer.masksToBounds = true
+        radiusView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        //viewModel.delegate = self
         
+    }
+    
+    @IBAction func backButtonPressed(_ sender: UIBarButtonItem) {
+        navigationController?.popViewController(animated: true)
+        //viewModel.delegate = self
     }
 }

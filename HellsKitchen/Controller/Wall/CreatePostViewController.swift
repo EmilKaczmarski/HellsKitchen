a9165ba5cb0a2596d77f85975128d53b9d4a99fe
EmilKaczmarski <51459339+EmilKaczmarski@users.noreply.github.com>
2020-06-15 23:40:01 +0200

    fixed bug in create postcontroller
    bug that has been fixed:
    now uploadedimageview is refreshing after adding post or quiting controller

--- HellsKitchen/Controller/Wall/CreatePostViewController.swift
+++ HellsKitchen/Controller/Wall/CreatePostViewController.swift
@@ -42,6 +42,7 @@ class CreatePostViewController: UIViewController, UIImagePickerControllerDelegat
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         tabBarController?.tabBar.isHidden = true
+        uploadedImage.image = nil
         hideStackViewOrEditingView()
         setupCancelButtonTitle()
         if Constants.currentUserName != "" {
@@ -71,18 +72,17 @@ class CreatePostViewController: UIViewController, UIImagePickerControllerDelegat
             self.tabBarController?.selectedIndex = 0
         }
     }
-  
+    
     
     @IBAction func nameTextChanged(_ sender: Any) {
-        
-    if !header.text!.isEmpty {
+        if !header.text!.isEmpty {
             nameLine.backgroundColor = Constants.Colors.deepGreen
         } else {
             nameLine.backgroundColor = Constants.Colors.lightGray
         }
         
-    if !header.text!.isEmpty && uploadedImage.image != nil {
-        enableAddRecipeButton()
+        if !header.text!.isEmpty && uploadedImage.image != nil {
+            enableAddRecipeButton()
         } else {
             disableAddRecipeButton()
         }
@@ -93,65 +93,63 @@ class CreatePostViewController: UIViewController, UIImagePickerControllerDelegat
     }
     
     func enableAddRecipeButton() {
-         addRecipeButton.isEnabled = true
-         buttonView.backgroundColor = Constants.Colors.deepGreen
+        addRecipeButton.isEnabled = true
+        buttonView.backgroundColor = Constants.Colors.deepGreen
     }
-     
+    
     func disableAddRecipeButton() {
-         addRecipeButton.isEnabled = false
-         buttonView.backgroundColor = Constants.Colors.deepGreenDisabled
+        addRecipeButton.isEnabled = false
+        buttonView.backgroundColor = Constants.Colors.deepGreenDisabled
     }
     
     //MARK: - Adding photo
     
     func photoPicker() {
-
-    let imagePickerController = UIImagePickerController()
-    imagePickerController.delegate = self
-    imagePickerController.allowsEditing = true
-    
-    let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
-    
-    actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
-        if UIImagePickerController.isSourceTypeAvailable(.camera){
-            imagePickerController.sourceType = .camera
-                      self.present(imagePickerController, animated: true, completion: nil)
-        } else {
-            print("Camera not available")
-        }
-      
-    }))
-    actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction) in
-        imagePickerController.sourceType = .photoLibrary
-        self.present(imagePickerController, animated: true, completion: nil)
-    }))
-    actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-    
-    self.present(actionSheet, animated: true, completion: nil)
-
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            } else {
+                print("Camera not available")
+            }
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+        
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-       
-           if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
-               uploadedImage.image = editedImage
-             } else if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-               uploadedImage.image = originalImage
-               
-           }
-           dismiss(animated: true, completion: nil)
+        
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            uploadedImage.image = editedImage
+        } else if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            uploadedImage.image = originalImage
+        }
+        hideStackViewOrEditingView()
+        dismiss(animated: true, completion: nil)
     }
-       
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-           picker.dismiss(animated: true, completion: nil)
-            hideStackViewOrEditingView()
+        picker.dismiss(animated: true, completion: nil)
+        hideStackViewOrEditingView()
     }
     
     @IBAction func addPhotoButtonPressed(_ sender: Any) {
         photoPicker()
         hideStackViewOrEditingView()
     }
-   
+    
     @IBAction func editingButtonPressed(_ sender: Any) {
         photoPicker()
         hideStackViewOrEditingView()
@@ -179,8 +177,8 @@ extension CreatePostViewController {
             infoStackView.isHidden = true
             editingView.isHidden = false
         } else {
-             infoStackView.isHidden = false
-             editingView.isHidden = true
+            infoStackView.isHidden = false
+            editingView.isHidden = true
         }
     }
 }
